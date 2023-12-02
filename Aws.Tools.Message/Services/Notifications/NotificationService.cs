a5b8f73ca4d3b9aca5199e054fe26859103f254e
pauloprsdesouza <paulo.prsdesouza@gmail.com>
2023-12-02 04:44:07 -0300

    Code Improvements

--- Aws.Tools.Message/Services/Notifications/NotificationService.cs
+++ Aws.Tools.Message/Services/Notifications/NotificationService.cs
@@ -19,84 +19,98 @@ namespace Aws.Tools.Message.Services.Notifications
     {
         private readonly IAmazonSimpleNotificationService _snsClient;
         private readonly ILogger<NotificationService> _logger;
-        private readonly string _baseSNSARN;
+        private readonly string _snsTopicArnPrefix;
 
-        public NotificationService(IAmazonSimpleNotificationService snsClient, ILogger<NotificationService> logger, IOptions<MessageConfiguration> messageConfiguration)
+        public NotificationService(IAmazonSimpleNotificationService snsClient, ILogger<NotificationService> logger, IOptions<MessageConfiguration> config)
         {
-            string region = messageConfiguration.Value.Region;
-            string accountId = messageConfiguration.Value.AccountId;
-
-            _baseSNSARN = $"arn:aws:sns:{region}:{accountId}:";
             _snsClient = snsClient;
             _logger = logger;
+            _snsTopicArnPrefix = $"arn:aws:sns:{config.Value.Region}:{config.Value.AccountId}:";
         }
 
-        private string GetTopicArn(string topicName) => _baseSNSARN + topicName;
+        private string ConstructTopicArn(string topicName) => _snsTopicArnPrefix + topicName;
 
         public async Task PublishMessageAsync<T>(string topicName, T message, string entityName = null, string apiName = null)
         {
-            Dictionary<string, MessageAttributeValue> attributes = new();
-
-            if (!string.IsNullOrEmpty(entityName))
-                attributes.Add("EntityName", new MessageAttributeValue() { DataType = "String", StringValue = entityName });
-
-            attributes.Add("ApiName", new MessageAttributeValue() { DataType = "String", StringValue = apiName ?? "Default" });
-
             try
             {
-                PublishRequest request = new()
+                var publishRequest = new PublishRequest
                 {
-                    TopicArn = GetTopicArn(topicName),
+                    TopicArn = ConstructTopicArn(topicName),
                     Message = JsonSerializer.Serialize(message, new JsonSerializerOptions().Default()),
-                    MessageAttributes = attributes ?? null
+                    MessageAttributes = ConstructMessageAttributes(entityName, apiName)
                 };
 
-                _ = await _snsClient.PublishAsync(request);
-
-                _logger.LogInformation("MESSAGE_HAS_BEEN_SENT TO: TopicName: {topicName} | EntityName: {entityName} | ApiName: {apiName}", topicName, entityName, apiName);
+                await _snsClient.PublishAsync(publishRequest);
+                _logger.LogInformation($"Message sent to {topicName} with Entity: {entityName}, API: {apiName}");
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "MESSAGE_HAS_NOT_BEEN_SENT");
+                _logger.LogError(ex, "Error sending message to SNS");
+                throw;
             }
         }
 
-        public async Task PublishSMSMessageAsync(string number, string message)
+        private Dictionary<string, MessageAttributeValue> ConstructMessageAttributes(string entityName, string apiName)
         {
-            var request = new PublishRequest
+            var attributes = new Dictionary<string, MessageAttributeValue>();
+            if (!string.IsNullOrEmpty(entityName))
             {
-                Message = message,
-                PhoneNumber = number
-            };
-
-            await _snsClient.PublishAsync(request);
+                attributes.Add("EntityName", new MessageAttributeValue { DataType = "String", StringValue = entityName });
+            }
+            attributes.Add("ApiName", new MessageAttributeValue { DataType = "String", StringValue = apiName ?? "Default" });
+            return attributes;
         }
 
-        public async Task SendEmailAsync(SESMessage message)
+        public async Task PublishSMSMessageAsync(string phoneNumber, string message)
         {
-            using AmazonSimpleEmailServiceClient client = new(RegionEndpoint.USEast1);
-            SendEmailRequest request = new()
+            try
             {
-                Source = message.SenderAddress,
-                Destination = new Destination
+                var request = new PublishRequest
                 {
-                    ToAddresses = message.ReceiversAddress
-                },
+                    PhoneNumber = phoneNumber,
+                    Message = message
+                };
+
+                await _snsClient.PublishAsync(request);
+                _logger.LogInformation($"SMS sent to {phoneNumber}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error sending SMS");
+                throw;
+            }
+        }
+
+        public async Task SendEmailAsync(SESMessage emailMessage)
+        {
+            try
+            {
+                using var emailClient = new AmazonSimpleEmailServiceClient(RegionEndpoint.USEast1);
+                var request = ConstructEmailRequest(emailMessage);
+
+                await emailClient.SendEmailAsync(request);
+                _logger.LogInformation("Email sent to: " + string.Join(", ", emailMessage.ReceiversAddress));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error sending email");
+                throw;
+            }
+        }
+
+        private SendEmailRequest ConstructEmailRequest(SESMessage emailMessage)
+        {
+            return new SendEmailRequest
+            {
+                Source = emailMessage.SenderAddress,
+                Destination = new Destination { ToAddresses = emailMessage.ReceiversAddress },
                 Message = new Amazon.SimpleEmail.Model.Message
                 {
-                    Subject = new Content(message.Subject),
-                    Body = new Body
-                    {
-                        Html = new Content
-                        {
-                            Charset = "UTF-8",
-                            Data = message.Body
-                        }
-                    }
+                    Subject = new Content(emailMessage.Subject),
+                    Body = new Body { Html = new Content { Charset = "UTF-8", Data = emailMessage.Body } }
                 }
             };
-
-            _ = await client.SendEmailAsync(request);
         }
 
         public async Task SendEmailAsync<T>(SESTemplateMessage<T> templateMessage)
